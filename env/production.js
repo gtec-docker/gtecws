5a63d5c3f29301cebd70494aefe3c7d6bac0544f
gtec-docker <34895149+gtec-docker@users.noreply.github.com>
2017-12-30 12:34:54 +0100

    Update production.js

--- env/production.js
+++ env/production.js
@@ -1,17 +1,18 @@
+
 module.exports = {
-		  user          : process.env.NODE_ORACLEDB_USER || "CC",
+		  user          : "gtec",
 
 		  // Instead of hard coding the password, consider prompting for it,
 		  // passing it in an environment variable via process.env, or using
 		  // External Authentication.
-		  password      : process.env.NODE_ORACLEDB_PASSWORD || "smart",
+		  password      : "gtec",
 
 		  // For information on connection strings see:
-		  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.1.8.18:1521/cc",
+		  host : "strutsWsDb",
+		  database: "testDb",
 
 		  // Setting externalAuth is optional. It defaults to false.  See:
 		  // https://github.com/oracle/node-oracledb/blob/master/doc/api.md#extauth
-		  externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false,
 				  http: {
 					  port: 3000
 				  },
